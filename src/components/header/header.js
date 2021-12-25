e23c95e57bfe9d22cb671586cefc6181762fc058
Tim Jackson <timjacksonm@gmail.com>
2021-12-25 10:33:27 -0600

    added styles

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./styles.css";
 
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <div className="header">
+      <h1 className="title">The Game Of Life</h1>
+    </div>
+  );
 };
 
 export default Header;
