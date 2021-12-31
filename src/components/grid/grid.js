1c1b54f16afd4d75466c446cb9363d9597d5350f
Tim Jackson <timjacksonm@gmail.com>
2021-12-30 18:37:19 -0600

    updated cell count slider functionality again. You can now increase the value and decrease the amount of cells while keeping the activated cells on the screen center based on their initial position. installed immer. installed uniqueid

--- src/components/grid/grid.js
+++ src/components/grid/grid.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import uniqid from 'uniqid';
+import { useImmer } from 'use-immer';
 import { useGrid } from '../hooks';
 import { create2dArray } from '../helperFunctions';
 import './styles.css';
 
 const Grid = ({ status, rows, columns }) => {
-  //20 different sizes. 80 cap. 4 interval
-
-  //on vars change update grid with additional rows columns
-  const [array, setArray] = useState([]);
+  const [array, setArray] = useImmer([]);
   useEffect(() => {
     //on mount fill default grid of rows and columns
     setArray(create2dArray(rows, columns));
@@ -18,13 +17,13 @@ const Grid = ({ status, rows, columns }) => {
       row: e.target.id.split(' ')[0],
       column: e.target.id.split(' ')[1],
     };
-    const arr = [...array];
-    arr[row][column] = e.target.className === 'cell' ? 1 : 0;
-    setArray(arr);
+    setArray((prevValue) => {
+      prevValue[row][column] = e.target.className === 'cell' ? 1 : 0;
+    });
   };
 
   function updateSize(template, size) {
-    const arrTemplate = template;
+    let arrTemplate = template;
     const start = (arrTemplate.length - array.length) / 2;
 
     //update 2dArray to larger size
@@ -37,95 +36,32 @@ const Grid = ({ status, rows, columns }) => {
           arrTemplate[i].splice(start, array.length, ...array[i - start]);
         }
       }
+      setArray(arrTemplate);
     }
     //update 2dArray to smaller size
     if (array.length > arrTemplate.length) {
-      //To Be Implemented
+      setArray((prevValue) => {
+        const arrayCopy = prevValue;
+        while (arrayCopy.length > size) {
+          arrayCopy.pop();
+          arrayCopy.shift();
+        }
+        for (let i = 0; i < arrayCopy.length; i++) {
+          while (arrayCopy[i].length > size) {
+            arrayCopy[i].pop();
+            arrayCopy[i].shift();
+          }
+        }
+        return arrayCopy;
+      });
     }
-    setArray(arrTemplate);
   }
 
   useEffect(() => {
-    console.log('rows,columns updated');
     //if template slice into current size
-    switch (rows) {
-      case '4':
-        updateSize(create2dArray(rows, columns), rows);
-        break;
-      case '8':
-        updateSize(create2dArray(rows, columns), rows);
-        break;
-      case '12':
-        updateSize(create2dArray(rows, columns), rows);
-        break;
-      case '16':
-        updateSize(create2dArray(rows, columns), rows);
-        break;
-      case '20':
-        updateSize(create2dArray(rows, columns), rows);
-        break;
-      case '24':
-        updateSize(create2dArray(rows, columns), rows);
-        break;
-      case '28':
-        updateSize(create2dArray(rows, columns), rows);
-        break;
-      case '32':
-        updateSize(create2dArray(rows, columns), rows);
-        break;
-      case '36':
-        updateSize(create2dArray(rows, columns), rows);
-        break;
-      case '40':
-        updateSize(create2dArray(rows, columns), rows);
-        break;
-      case '44':
-        updateSize(create2dArray(rows, columns), rows);
-        break;
-      case '48':
-        updateSize(create2dArray(rows, columns), rows);
-        break;
-      case '52':
-        updateSize(create2dArray(rows, columns), rows);
-        break;
-      case '56':
-        updateSize(create2dArray(rows, columns), rows);
-        break;
-      case '60':
-        updateSize(create2dArray(rows, columns), rows);
-        break;
-      case '64':
-        updateSize(create2dArray(rows, columns), rows);
-        break;
-      case '68':
-        updateSize(create2dArray(rows, columns), rows);
-        break;
-      case '72':
-        updateSize(create2dArray(rows, columns), rows);
-        break;
-      case '76':
-        updateSize(create2dArray(rows, columns), rows);
-        break;
-      case '80':
-        updateSize(create2dArray(rows, columns), rows);
-        break;
-      default:
-        break;
-    }
+    updateSize(create2dArray(rows, columns), rows);
   }, [rows, columns]);
 
-  // const { array, defineCell } = useGrid(rows, columns);
-  // const create2dArray = (rows, columns) => {
-  //   const myArray = [];
-
-  //   for (let i = 0; i < rows; i++) {
-  //     myArray[i] = [];
-  //     for (let j = 0; j < columns; j++) {
-  //       myArray[i][j] = 0;
-  //     }
-  //   }
-  //   return myArray;
-  // };
   return (
     <div
       className="container"
@@ -139,7 +75,7 @@ const Grid = ({ status, rows, columns }) => {
           return (
             <div
               className={cell === 0 ? 'cell' : 'cell active'}
-              key={`${rowIndex}${cellIndex}`}
+              key={uniqid()}
               id={`${rowIndex} ${cellIndex}`}
               onClick={defineCell}
             ></div>
