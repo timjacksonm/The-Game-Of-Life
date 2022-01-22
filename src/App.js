7ffedbe2102e3c425cc22ac55066c6f920398d8c
Tim Jackson <timjacksonm@gmail.com>
2022-01-22 15:56:42 -0600

    created drawGrid function and simplified it a bit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 
 const App = () => {
   const canvasRef = useRef(null);
-  const cellSize = 16;
+  const cellSize = 15;
   const gridGap = 1;
 
   const [rows, setRows] = useState(
@@ -14,24 +14,24 @@ const App = () => {
 
   const defaultGrid = new Array(cols)
     .fill(0)
-    .map(() => new Array(rows).fill(0));
+    .map(() =>
+      new Array(rows).fill(null).map(() => Math.floor(Math.random() * 2))
+    );
 
-  const drawCell = (ctx, cell, index) => {
-    const x = index.row * cellSize + index.row;
-    const y = index.column * (cellSize + gridGap);
-    ctx.fillStyle = cell === 1 ? '#00adb5' : '#393e46';
-    ctx.fillRect(x, y, cellSize, cellSize);
-  };
+  function drawGrid(grid, ctx) {
+    for (let i = 0; i < grid.length; i++) {
+      for (let k = 0; k < grid[i].length; k++) {
+        const x = k * cellSize + k;
+        const y = i * (cellSize + gridGap);
+        ctx.fillStyle = grid[i][k] === 1 ? '#00adb5' : '#393e46';
+        ctx.fillRect(x, y, cellSize, cellSize);
+      }
+    }
+  }
 
   useEffect(() => {
     const ctx = canvasRef.current.getContext('2d');
-
-    for (let i = 0; i < defaultGrid.length; i++) {
-      for (let k = 0; k < defaultGrid[i].length; k++) {
-        const cellValue = defaultGrid[i][k];
-        drawCell(ctx, cellValue, { column: i, row: k });
-      }
-    }
+    drawGrid(defaultGrid, ctx);
   });
   return (
     <>
