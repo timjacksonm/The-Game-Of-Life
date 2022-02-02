451a3988a347d86097092a2fded4d8aa4a021627
Tim Jackson <timjacksonm@gmail.com>
2022-02-02 07:55:44 -0600

    refactoring for performance. Going to reduce the amount of loops and define the drawGrid and drawHover in the same loop. commented out code and starting fresh

--- src/components/canvas/canvas.js
+++ src/components/canvas/canvas.js
@@ -17,7 +17,7 @@ const Canvas = ({
 }) => {
   const canvasRef = useRef(null);
   const [mouseCoords, setMouseCoords] = useState();
-  const [liveCoords, setLiveCoords] = useState();
+  const [liveCoords, setLiveCoords] = useState(() => new Set());
 
   //standard throughout document = grid[x][y]
   //x representing entire horizontal row. y representing vertical value in row.
@@ -50,16 +50,6 @@ const Canvas = ({
     return num;
   }
 
-  function updateCell(x, y, value) {
-    setGrid((gridCopy) =>
-      gridCopy.map((row, index) =>
-        index !== x
-          ? row
-          : row.map((cell, index) => (index !== y ? cell : value))
-      )
-    );
-  }
-
   function nextGen() {
     setGrid((gridCopy) => {
       for (let x = 0; x < grid.length; x++) {
@@ -81,30 +71,19 @@ const Canvas = ({
     setGenCount((prevValue) => prevValue + 1);
   }
 
-  function drawGrid(grid, ctx) {
-    for (let x = 0; x < grid.length; x++) {
-      for (let y = 0; y < grid[x].length; y++) {
-        const coordX = y * cellSize + y;
-        const coordY = x * (cellSize + gridGap);
-        ctx.fillStyle = grid[x][y] === 1 ? color : '#393e46';
-        ctx.fillRect(coordX, coordY, cellSize, cellSize);
-      }
-    }
-  }
-
-  function handleClick(e) {
-    if (liveCoords) {
-      liveCoords.forEach((pair) => {
-        const cellValue = grid[pair[0]][pair[1]] ? 0 : 1;
-        updateCell(pair[0], pair[1], cellValue);
-      });
-    } else {
-      const x = Math.floor(e.pageY / (cellSize + gridGap));
-      const y = Math.floor(e.pageX / (cellSize + gridGap));
-      const cellValue = grid[x][y] ? 0 : 1;
-      updateCell(x, y, cellValue);
-    }
-  }
+  // function handleClick(e) {
+  //   if (liveCoords) {
+  //     liveCoords.forEach((pair) => {
+  //       const cellValue = grid[pair[0]][pair[1]] ? 0 : 1;
+  //       updateCell(pair[0], pair[1], cellValue);
+  //     });
+  //   } else {
+  //     const x = Math.floor(e.pageY / (cellSize + gridGap));
+  //     const y = Math.floor(e.pageX / (cellSize + gridGap));
+  //     const cellValue = grid[x][y] ? 0 : 1;
+  //     updateCell(x, y, cellValue);
+  //   }
+  // }
 
   function decreaseGrid() {
     const newGrid = defaultGrid(windowSize, gridGap, cellSize);
@@ -183,60 +162,77 @@ const Canvas = ({
     }
   });
 
-  function getCoords(array, center) {
-    const coordArray = [];
-    for (let x = 0; x < array.length; x++) {
-      for (let y = 0; y < array[x].length; y++) {
-        if ([x, y] === center && array[x][y]) {
-          coordArray.push([x, y]);
-          continue;
-        }
+  // function getCoords(array, center) {
+  //   const coordArray = [];
+  //   for (let x = 0; x < array.length; x++) {
+  //     for (let y = 0; y < array[x].length; y++) {
+  //       if ([x, y] === center && array[x][y]) {
+  //         coordArray.push([x, y]);
+  //         continue;
+  //       }
 
-        if ([x, y] !== center && array[x][y] === 1)
-          coordArray.push([x - center[0], y - center[1]]);
-      }
-    }
-    return coordArray;
+  //       if ([x, y] !== center && array[x][y] === 1)
+  //         coordArray.push([x - center[0], y - center[1]]);
+  //     }
+  //   }
+  //   return coordArray;
+  // }
+
+  // function drawHover(cmx) {
+  //   if (liveCoords) {
+  //     liveCoords.forEach((pair) => {
+  //       const coordX = pair[1] * cellSize + pair[1];
+  //       const coordY = pair[0] * (cellSize + gridGap);
+  //       cmx.fillStyle = color;
+  //       cmx.fillRect(coordX, coordY, cellSize, cellSize);
+  //     });
+  //   }
+  // }
+
+  // function getAlivePairs(mouseX, mouseY) {
+  //   if (brush) {
+  //     const x = Math.floor(mouseY / (cellSize + gridGap));
+  //     const y = Math.floor(mouseX / (cellSize + gridGap));
+  //     const brushX = Math.floor(brush.length / 2);
+  //     const brushY = Math.floor(brush[brushX].length / 2);
+  //     const coords = getCoords(brush, [brushX, brushY]);
+
+  //     return coords.map((pair) => [x + pair[0], y + pair[1]]);
+  //   }
+  // }
+
+  // function handleMouseMove(e) {
+  //   if (!mouseCoords) {
+  //     setMouseCoords([e.pageX, e.pageY]);
+  //   } else if (
+  //     mouseCoords[0] >= e.pageX + gridGap + cellSize ||
+  //     e.pageX - (gridGap + cellSize) >= mouseCoords[0] ||
+  //     mouseCoords[1] >= e.pageY + gridGap + cellSize ||
+  //     e.pageY - (gridGap + cellSize) >= mouseCoords[1]
+  //   ) {
+  //     setMouseCoords([e.pageX, e.pageY]);
+  //     const pairs = getAlivePairs(e.pageX, e.pageY);
+  //     setLiveCoords(() => new Set(pairs));
+  //   }
+  // }
+
+  function updateCell(x, y, value) {
+    setGrid((gridCopy) =>
+      gridCopy.map((row, index) =>
+        index !== x
+          ? row
+          : row.map((cell, index) => (index !== y ? cell : value))
+      )
+    );
   }
 
-  function drawHover(cmx) {
-    if (liveCoords) {
-      liveCoords.forEach((pair) => {
-        const coordX = pair[1] * cellSize + pair[1];
-        const coordY = pair[0] * (cellSize + gridGap);
-        cmx.fillStyle = color;
-        cmx.fillRect(coordX, coordY, cellSize, cellSize);
-      });
-    }
-  }
-
-  function getAlivePairs(mouseX, mouseY) {
-    if (brush) {
-      const x = Math.floor(mouseY / (cellSize + gridGap));
-      const y = Math.floor(mouseX / (cellSize + gridGap));
-      const brushX = Math.floor(brush.length / 2);
-      const brushY = Math.floor(brush[brushX].length / 2);
-      const coords = getCoords(brush, [brushX, brushY]);
-
-      return coords.map((pair) => [x + pair[0], y + pair[1]]);
-    }
-  }
-
-  function handleMouseMove(e) {
-    if (!start) {
-      if (!mouseCoords) {
-        setMouseCoords([e.pageX, e.pageY]);
-      } else {
-        if (
-          mouseCoords[0] >= e.pageX + gridGap + cellSize ||
-          e.pageX - (gridGap + cellSize) >= mouseCoords[0] ||
-          mouseCoords[1] >= e.pageY + gridGap + cellSize ||
-          e.pageY - (gridGap + cellSize) >= mouseCoords[1]
-        ) {
-          setMouseCoords([e.pageX, e.pageY]);
-          const pairs = getAlivePairs(e.pageX, e.pageY);
-          setLiveCoords(pairs);
-        }
+  function drawGrid(grid, ctx) {
+    for (let x = 0; x < grid.length; x++) {
+      for (let y = 0; y < grid[x].length; y++) {
+        const coordX = y * cellSize + y;
+        const coordY = x * (cellSize + gridGap);
+        ctx.fillStyle = grid[x][y] === 1 ? color : '#393e46';
+        ctx.fillRect(coordX, coordY, cellSize, cellSize);
       }
     }
   }
@@ -245,8 +241,12 @@ const Canvas = ({
     const ctx = canvasRef.current.getContext('2d');
     ctx.clearRect(0, 0, windowSize.width, windowSize.height);
     drawGrid(grid, ctx);
-    if (!start) drawHover(ctx);
   });
+
+  // useEffect(() => {
+  //   const ctx = canvasRef.current.getContext('2d');
+  //   drawHover(ctx);
+  // }, [liveCoords, drawHover]);
 
   useEffect(() => {
     const count = grid.flat().filter((num) => num === 1).length;
@@ -261,8 +261,8 @@ const Canvas = ({
         ref={canvasRef}
         width={window.innerWidth}
         height={window.innerHeight}
-        onClick={handleClick}
-        onMouseMove={handleMouseMove}
+        // onClick={handleClick}
+        // onMouseMove={handleMouseMove}
       />
     </>
   );
