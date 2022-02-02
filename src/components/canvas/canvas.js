62e4a3fdecc86496857b910fccdc28d90b8ff950
Tim Jackson <timjacksonm@gmail.com>
2022-02-02 10:43:38 -0600

    reverted to original double canvas. disabling hover when start. Seems to be best solution for performance so far.

--- src/components/canvas/canvas.js
+++ src/components/canvas/canvas.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState, useCallback } from 'react';
 import useInterval from '../../hooks/useInterval';
 import defaultGrid from '../../utils';
 
@@ -16,6 +16,7 @@ const Canvas = ({
   color,
 }) => {
   const canvasRef = useRef(null);
+  const hoverRef = useRef(null);
   const [mouseCoords, setMouseCoords] = useState();
   const [liveCoords, setLiveCoords] = useState(() => new Set());
 
@@ -81,19 +82,30 @@ const Canvas = ({
     setGenCount((prevValue) => prevValue + 1);
   }
 
-  // function handleClick(e) {
-  //   if (liveCoords) {
-  //     liveCoords.forEach((pair) => {
-  //       const cellValue = grid[pair[0]][pair[1]] ? 0 : 1;
-  //       updateCell(pair[0], pair[1], cellValue);
-  //     });
-  //   } else {
-  //     const x = Math.floor(e.pageY / (cellSize + gridGap));
-  //     const y = Math.floor(e.pageX / (cellSize + gridGap));
-  //     const cellValue = grid[x][y] ? 0 : 1;
-  //     updateCell(x, y, cellValue);
-  //   }
-  // }
+  function drawGrid(grid, ctx) {
+    for (let x = 0; x < grid.length; x++) {
+      for (let y = 0; y < grid[x].length; y++) {
+        const coordX = y * cellSize + y;
+        const coordY = x * (cellSize + gridGap);
+        ctx.fillStyle = grid[x][y] === 1 ? color : '#393e46';
+        ctx.fillRect(coordX, coordY, cellSize, cellSize);
+      }
+    }
+  }
+
+  function handleClick(e) {
+    if (liveCoords) {
+      liveCoords.forEach((pair) => {
+        const cellValue = grid[pair[0]][pair[1]] ? 0 : 1;
+        updateCell(pair[0], pair[1], cellValue);
+      });
+    } else {
+      const x = Math.floor(e.pageY / (cellSize + gridGap));
+      const y = Math.floor(e.pageX / (cellSize + gridGap));
+      const cellValue = grid[x][y] ? 0 : 1;
+      updateCell(x, y, cellValue);
+    }
+  }
 
   function decreaseGrid() {
     const newGrid = defaultGrid(windowSize, gridGap, cellSize);
@@ -172,84 +184,73 @@ const Canvas = ({
     }
   });
 
-  // function getCoords(array, center) {
-  //   const coordArray = [];
-  //   for (let x = 0; x < array.length; x++) {
-  //     for (let y = 0; y < array[x].length; y++) {
-  //       if ([x, y] === center && array[x][y]) {
-  //         coordArray.push([x, y]);
-  //         continue;
-  //       }
+  function getCoords(array, center) {
+    const coordArray = [];
+    for (let x = 0; x < array.length; x++) {
+      for (let y = 0; y < array[x].length; y++) {
+        if ([x, y] === center && array[x][y]) {
+          coordArray.push([x, y]);
+          continue;
+        }
 
-  //       if ([x, y] !== center && array[x][y] === 1)
-  //         coordArray.push([x - center[0], y - center[1]]);
-  //     }
-  //   }
-  //   return coordArray;
-  // }
+        if ([x, y] !== center && array[x][y] === 1)
+          coordArray.push([x - center[0], y - center[1]]);
+      }
+    }
+    return coordArray;
+  }
 
-  // function drawHover(cmx) {
-  //   if (liveCoords) {
-  //     liveCoords.forEach((pair) => {
-  //       const coordX = pair[1] * cellSize + pair[1];
-  //       const coordY = pair[0] * (cellSize + gridGap);
-  //       cmx.fillStyle = color;
-  //       cmx.fillRect(coordX, coordY, cellSize, cellSize);
-  //     });
-  //   }
-  // }
+  function drawHover(cmx) {
+    if (liveCoords) {
+      liveCoords.forEach((pair) => {
+        const coordX = pair[1] * cellSize + pair[1];
+        const coordY = pair[0] * (cellSize + gridGap);
+        cmx.fillStyle = color;
+        cmx.fillRect(coordX, coordY, cellSize, cellSize);
+      });
+    }
+  }
 
-  // function getAlivePairs(mouseX, mouseY) {
-  //   if (brush) {
-  //     const x = Math.floor(mouseY / (cellSize + gridGap));
-  //     const y = Math.floor(mouseX / (cellSize + gridGap));
-  //     const brushX = Math.floor(brush.length / 2);
-  //     const brushY = Math.floor(brush[brushX].length / 2);
-  //     const coords = getCoords(brush, [brushX, brushY]);
+  function getAlivePairs(mouseX, mouseY) {
+    if (brush) {
+      const x = Math.floor(mouseY / (cellSize + gridGap));
+      const y = Math.floor(mouseX / (cellSize + gridGap));
+      const brushX = Math.floor(brush.length / 2);
+      const brushY = Math.floor(brush[brushX].length / 2);
+      const coords = getCoords(brush, [brushX, brushY]);
 
-  //     return coords.map((pair) => [x + pair[0], y + pair[1]]);
-  //   }
-  // }
+      return coords.map((pair) => [x + pair[0], y + pair[1]]);
+    }
+  }
 
-  // function handleMouseMove(e) {
-  //   if (!mouseCoords) {
-  //     setMouseCoords([e.pageX, e.pageY]);
-  //   } else if (
-  //     mouseCoords[0] >= e.pageX + gridGap + cellSize ||
-  //     e.pageX - (gridGap + cellSize) >= mouseCoords[0] ||
-  //     mouseCoords[1] >= e.pageY + gridGap + cellSize ||
-  //     e.pageY - (gridGap + cellSize) >= mouseCoords[1]
-  //   ) {
-  //     setMouseCoords([e.pageX, e.pageY]);
-  //     const pairs = getAlivePairs(e.pageX, e.pageY);
-  //     setLiveCoords(() => new Set(pairs));
-  //   }
-  // }
-
-  // function drawHoverCell(ctx, x, y) {}
-
-  function drawCell(ctx, x, y) {
-    const coordX = y * cellSize + y;
-    const coordY = x * (cellSize + gridGap);
-    ctx.fillStyle = grid[x][y] === 1 ? color : '#393e46';
-    ctx.fillRect(coordX, coordY, cellSize, cellSize);
+  function handleMouseMove(e) {
+    if (!mouseCoords) {
+      setMouseCoords([e.pageX, e.pageY]);
+    } else if (
+      mouseCoords[0] >= e.pageX + gridGap + cellSize ||
+      e.pageX - (gridGap + cellSize) >= mouseCoords[0] ||
+      mouseCoords[1] >= e.pageY + gridGap + cellSize ||
+      e.pageY - (gridGap + cellSize) >= mouseCoords[1]
+    ) {
+      setMouseCoords([e.pageX, e.pageY]);
+      const pairs = getAlivePairs(e.pageX, e.pageY);
+      setLiveCoords(() => new Set(pairs));
+    }
   }
 
   useEffect(() => {
     const ctx = canvasRef.current.getContext('2d');
     ctx.clearRect(0, 0, windowSize.width, windowSize.height);
-    for (let x = 0; x < grid.length; x++) {
-      for (let y = 0; y < grid[x].length; y++) {
-        drawCell(ctx, x, y);
-        // drawHoverCell(ctx, x, y)
-      }
-    }
+    drawGrid(grid, ctx);
   });
 
-  // useEffect(() => {
-  //   const ctx = canvasRef.current.getContext('2d');
-  //   drawHover(ctx);
-  // }, [liveCoords, drawHover]);
+  useEffect(() => {
+    if (!start) {
+      const cxm = hoverRef.current.getContext('2d');
+      cxm.clearRect(0, 0, windowSize.width, windowSize.height);
+      drawHover(cxm);
+    }
+  });
 
   useEffect(() => {
     const count = grid.flat().filter((num) => num === 1).length;
@@ -264,9 +265,17 @@ const Canvas = ({
         ref={canvasRef}
         width={window.innerWidth}
         height={window.innerHeight}
-        // onClick={handleClick}
-        // onMouseMove={handleMouseMove}
       />
+      {!start && (
+        <canvas
+          className="absolute top-0 m-auto"
+          ref={hoverRef}
+          width={window.innerWidth}
+          height={window.innerHeight}
+          onClick={handleClick}
+          onMouseMove={handleMouseMove}
+        />
+      )}
     </>
   );
 };
