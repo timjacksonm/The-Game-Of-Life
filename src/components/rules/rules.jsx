92d4a6f4a007ffd3a57ffd4b9ca21f95ffe1c1f1
Tim Jackson <timjacksonm@gmail.com>
2022-02-20 10:41:01 -0600

    added rules and gifs to sidenav

--- src/components/rules/rules.jsx
+++ src/components/rules/rules.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import example1 from '../../assets/examplegif1.gif';
+import example2 from '../../assets/examplegif2.gif';
+import example3 from '../../assets/examplegif3.gif';
+import example4 from '../../assets/examplegif4.gif';
 
 const Rules = () => {
-  return <div></div>;
+  return (
+    <div className="bg-gray-600 p-3 m-3">
+      <h1>Rules:</h1>
+      <p>
+        1. Any live cell with fewer than two live neighbours dies, as if by
+        underpopulation.
+      </p>
+      <img src={example1} alt="test" />
+      <p>
+        2. Any live cell with two or three live neighbours lives on to the next
+        generation.
+      </p>
+      <img src={example2} alt="test" />
+      <p>
+        3. Any live cell with more than three live neighbours dies, as if by
+        overpopulation.
+      </p>
+      <img src={example3} alt="test" />
+      <p>
+        4. Any dead cell with exactly three live neighbours becomes a live cell,
+        as if by reproduction.
+      </p>
+      <img src={example4} alt="test" />
+    </div>
+  );
 };
 
 export default Rules;
